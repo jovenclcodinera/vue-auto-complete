d093ad45d9c3fd0702d4246ac70e41ad7b844875
User <joven_deogracias@yahoo.com>
2020-03-23 22:04:15 +0800

    Vue.js - From The Ground Up

---
+++ src/views/Forms.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Telephone template="(XXX) XXX-XXXX"></Telephone>
+    </div>
+</template>
+
+<script>
+    import Telephone from "../components/Telephone";
+
+    export default {
+        name: "Forms",
+        components: {
+            Telephone
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
